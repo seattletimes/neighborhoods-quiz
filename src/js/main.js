411aff828436ebd84cfee945d4bda630d49ef65f
Audrey Carlsen <acarlsen@seattletimes.com>
2016-11-09 15:59:38 -0800

    calculates top three results

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 require("./lib/social");
 require("./lib/ads");
+var shuffle = require("lodash.shuffle");
 var initGallery = require("./lib/gallery.js");
 var track = require("./lib/tracking");
 
@@ -10,7 +11,7 @@ var Share = require("share");
 var questionTemplate = require("./_questionTemplate.html");
 var overviewTemplate = require("./_overviewTemplate.html");
 var galleryTemplate = require("../partials/_gallery.html");
-var additionalResultsTemplate = require("../partials/_additionalResults.html");
+var additionalResultsTemplate = require("./_additionalResults.html");
 
 // Set up templates
 ich.addTemplate("questionTemplate", questionTemplate);
@@ -45,92 +46,116 @@ var watchInput = function() {
   }));
 };
 
-var id = 8; //question number
+var id = 7; //question number
 
+var scores = {};
 
+$(".question-box").on("click", ".submit", function() {
+    // score answer
+    var options = $("input:checked").val().split(" ");
+    options.forEach(function(option) {
+      if (option === "") return;
+      if (!scores[option]) { scores[option] = 0 }
+      scores[option] += 1;
+    });
 
-  var scores = {};
+    if (id < Object.keys(quizData).length) {
+      // move on to next question
+      id += 1;
+      showQuestion(id);
+      $(".submit").removeClass("active");
+      $(".submit").attr("disabled", true);
+      // Change button text on last question
+      if (id == Object.keys(quizData).length) {
+        $(".submit").html("Finish");
+      }
+    } else {
+      calculateResult();
+    }
+  });
 
-  $(".question-box").on("click", ".submit", function() {
-      // score answer
-      var options = $("input:checked").val().split(" ");
-      options.forEach(function(option) {
-        if (option === "") return;
-        if (!scores[option]) { scores[option] = 0 }
-        scores[option] += 1;
+var calculateResult = function() {
+  // find highest match(es)
+  var options = [];
+  for (var option in scores) {
+    options.push({
+      name: option,
+      score: scores[option]
+    });
+  }
+
+  options.sort((a,b) => b.score - a.score);
+
+  var highest = [];
+  var top = [];
+  var target = options[0].score; //highest score
+  for (var i = 0; i < options.length; i++) {
+    var o = options[i];
+    if (o.score == target) {
+      top.push(o);
+      highest.push(o);
+    } else if (highest.length < 3) {
+      highest.push(o);
+    }
+  }
+
+  var final;
+
+  if (top.length >= 3) {
+    final = shuffle(top).slice(0, 3);
+  } else if (top.length == 2) {
+    final = shuffle(top);
+    final.push(highest[2]);
+  } else {
+    final = highest;
+  }
+
+  final = final.map(function(o) {
+    for (var i = 0; i < resultsData.length; i++) {
+      var option = resultsData[i];
+      if (option.id == o.name) return option;
+    };
+  });
+
+  var first = final.shift();
+
+  displayResult(first, final);
+
+  new Share(".share-button", {
+        description: "I got " + first.title + "! Which Seattle-area neighborhood are you?" + document.querySelector(`meta[property="og:description"]`).innerHTML,
+        ui: {
+          flyout: "bottom right",
+          button_text: "Share results"
+        },
+        networks: {
+          email: {
+            description: "I got " + first.title + "! Which Seattle-area neighborhood are you?" + [document.querySelector(`meta[property="og:description"]`).innerHTML, window.location.href].join("\n")
+          }
+        }
       });
 
-      if (id < Object.keys(quizData).length) {
-        // move on to next question
-        id += 1;
-        showQuestion(id);
-        $(".submit").removeClass("active");
-        $(".submit").attr("disabled", true);
-        // Change button text on last question
-        if (id == Object.keys(quizData).length) {
-          $(".submit").html("Finish");
-        }
-      } else {
-        calculateResult();
-      }
-    });
+  $(".share-button").addClass("share-results");
+};
 
-  var calculateResult = function() {
-    // find highest match(es)
-    var highestScore = 0;
-    for (var option in scores) {
-      if (scores[option] >= highestScore) {
-        highestScore = scores[option];
-      }
-    }
 
-    //loop again to find ties
-    var highestOptions = [];
-    for (var option in scores) {
-      if (scores[option] == highestScore) {
-        highestOptions.push(option);
-      }
-      console.log(highestOptions)
-    }
+var displayResult = function(first, final) {
+  $(".quiz-box").html(ich.overviewTemplate(first));
 
-    var random = Math.round(Math.random() * (highestOptions.length - 1));
-    var resultId = highestOptions[random];
-    var result;
-    resultsData.forEach(function(option) {
-      if (option.id != resultId) return;
-      result = option;
-    });
+  var photos = galleryData.filter(function(c) {
+     return c.gallery == first.title;
+  });
+  photos.forEach((p, i) => p.index = i)
+  photos[0].first = true;
 
-    // display result
-    $(".quiz-box").html(ich.overviewTemplate(result));
+  $(".results-image").html(ich.galleryTemplate({photos: photos, length: photos.length}));
+  initGallery();
 
-    var photos = galleryData.filter(function(c) {
-       return c.gallery == result.title;
-    });
-    photos.forEach((p, i) => p.index = i)
-    photos[0].first = true;
+  console.log(final)
 
-    $(".results-image").html(ich.galleryTemplate({photos: photos, length: photos.length}));
-    initGallery();
+  $(".additional-results").html(ich.additionalResultsTemplate({alt: final}));
 
-    $(".additional-results").html(ich.additionalResultsTemplate());
-
-    $(".retake").removeClass("hidden");
-    new Share(".share-button", {
-          description: "I got " + result.title + "! Which Seattle-area neighborhood are you?" + document.querySelector(`meta[property="og:description"]`).innerHTML,
-          ui: {
-            flyout: "bottom right",
-            button_text: "Share results"
-          },
-          networks: {
-            email: {
-              description: "I got " + result.title + "! Which Seattle-area neighborhood are you?" + [document.querySelector(`meta[property="og:description"]`).innerHTML, window.location.href].join("\n")
-            }
-          }
-        });
-
-    $(".share-button").addClass("share-results");
-  };
+  $(".retake").removeClass("hidden");
+};
 
 
 
